0a7b4bc914a88a0aa7dd4ae11f2ab2460c13fe3f
Konrad Siwak <8471540-konrad.siwak@users.noreply.gitlab.com>
2024-06-22 16:46:05 +0200

    przeniesienie plików Scrapera local'nego do osobnego modułu

---
+++ controllers/scrapeLocal.go
@@ -0,0 +1,60 @@
+package controllers
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/gocolly/colly"
+)
+
+func ScrapeLocalData() {
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		panic(err)
+	}
+
+	t := &http.Transport{}
+	t.RegisterProtocol("file", http.NewFileTransport(http.Dir("/")))
+
+	c := colly.NewCollector()
+	c.WithTransport(t)
+
+	c.OnHTML("a.persona_name_text_content", func(h *colly.HTMLElement) {
+		var UserName string
+		UserName = h.Text
+		UserName = strings.TrimSpace(UserName)
+
+		fmt.Println("=================")
+		fmt.Println("Gracz:", UserName)
+		fmt.Println("=================")
+
+	})
+
+	c.OnHTML("._22awlPiAoaZjQMqxJhp-KP", func(e *colly.HTMLElement) {
+		Title := e.Text
+		GameURL := e.Attr("href")
+		GameID := strings.ReplaceAll(GameURL, "https://store.steampowered.com/app/", "")
+		GameID = strings.TrimSpace(GameID)
+		fmt.Println("--------------------------------------")
+		fmt.Println("Tytuł:", Title)
+		fmt.Println("ID gry:", GameID)
+		fmt.Println("GameURL:", GameURL)
+		fmt.Println("--------------------------------------")
+	})
+
+	FileList, err := os.ReadDir("./data")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, file := range FileList {
+		// fmt.Println(file.Name())
+
+		c.Visit("file://" + dir + "/data/" + file.Name())
+		fmt.Println("file://" + dir + "/data/" + file.Name())
+	}
+
+}
--- main.go
+++ main.go
@@ -1,64 +1,18 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-	"os"
-	"path/filepath"
-	"strings"
+	// "os"
+	// "path/filepath"
 
-	"github.com/gocolly/colly"
+	"steamFriendsGames/controllers"
+	"steamFriendsGames/initialisers"
 )
 
+func init() {
+	initialisers.ConnectToDataBase()
+	initialisers.SyncDB()
+}
+
 func main() {
-	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
-	if err != nil {
-		panic(err)
-	}
-
-	t := &http.Transport{}
-	t.RegisterProtocol("file", http.NewFileTransport(http.Dir("/")))
-
-	c := colly.NewCollector()
-	c.WithTransport(t)
-
-	// pages := []string{}
-
-	c.Visit("file://" + dir + "/data/view-source_https___steamcommunity.com_id_aquiceeo_games__tab=all.html")
-	println("file://" + dir + "/data/view-source_https___steamcommunity.com_id_aquiceeo_games__tab=all.html")
-	c.OnHTML("a.persona_name_text_content", func(h *colly.HTMLElement) {
-		var UserName string
-		UserName = h.Text
-		UserName = strings.TrimSpace(UserName)
-
-		fmt.Println("=================")
-		fmt.Println("Gracz:", UserName)
-		fmt.Println("=================")
-
-	})
-
-	c.OnHTML("._22awlPiAoaZjQMqxJhp-KP", func(e *colly.HTMLElement) {
-		Title := e.Text
-		GameURL := e.Attr("href")
-		GameID := strings.ReplaceAll(GameURL, "https://store.steampowered.com/app/", "")
-		GameID = strings.TrimSpace(GameID)
-		fmt.Println("--------------------------------------")
-		fmt.Println("Tytuł:", Title)
-		fmt.Println("ID gry:", GameID)
-		fmt.Println("GameURL:", GameURL)
-		fmt.Println("--------------------------------------")
-	})
-
-	FileList, err := os.ReadDir("./data")
-	if err != nil {
-		log.Fatal(err)
-	}
-	for _, file := range FileList {
-		// fmt.Println(file.Name())
-
-		c.Visit("file://" + dir + "/data/" + file.Name())
-		fmt.Println("file://" + dir + "/data/" + file.Name())
-	}
-
+	controllers.ScrapeLocalData()
 }

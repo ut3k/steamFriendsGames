de2f546261bae4e5958f89d758ca6fdeab6ebf54
Konrad Siwak <8471540-konrad.siwak@users.noreply.gitlab.com>
2024-06-25 19:11:21 +0200

    przygotowanie plików do sprawdzanie czy gra jest coop

---
+++ controllers/checkcoop.go
@@ -0,0 +1,28 @@
+package controllers
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"steamFriendsGames/models"
+
+	"github.com/gocolly/colly"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func CheckIfGameIsCoop() {
+	var err error
+	var DB *gorm.DB
+	DB, err = gorm.Open(sqlite.Open("data.db"), &gorm.Config{})
+	if err != nil {
+		fmt.Println("Function CheckIfGameIsCoop can not connect to data.db")
+	}
+
+	c := colly.NewCollector()
+	c.OnHTML("")
+}
